463dcce545d5957aac8d65b778c5abb4d55b57d3
Lucas Jesus <cc19372@g.unicamp.br>
2020-12-07 17:57:13 -0300

    com o texto final

--- src/Arquivo.java
+++ src/Arquivo.java
@@ -71,9 +71,9 @@ public class Arquivo {
                 for (CodigoBinario cod : textoInOrdem) {
                     myWriter.write(cod.getSequenciaBinaria());
                 }
-//                for (CodigoBinario cod: textoInOrdem) {
-//                    myWriter.write(cod.getCaracter());
-//                }
+                for (CodigoBinario cod: textoInOrdem) {
+                    myWriter.write(cod.getCaracter());
+                }
                 myWriter.close();
             } else {
                 final FileWriter myWriter = new FileWriter(test.getName());
